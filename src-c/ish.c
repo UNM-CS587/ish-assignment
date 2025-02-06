c244cddbfef4ff0088e560af41ff02a2886d7653
Charbar007 <charleymbickel@gmail.com>
2025-02-06 23:12:21 +0000

    Implemented built-in command handler logic

--- src-c/ish.c
+++ src-c/ish.c
@@ -1,14 +1,50 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 
 #include "command.h"
-#include "parser.tab.h"
+#include "parser.h"
+
+char name[256];
 
 void
 issuePrompt(FILE *pf)
 {
-    fprintf(pf, "> ");
+    fprintf(pf, "%s> ", name);
     fflush(pf);
+}
+
+void applyRedirection(command *cmd){
+
+}
+
+void runCommand(command *cmd){
+
+}
+
+void changeDirectory(command *cmd){
+    printf("command: ");
+    printCommand(cmd, stdout);
+    printf("\nArguments:");
+    printArguments(cmd->pa, stdout);
+    printf("\n");
+}
+
+void setEnVars(){
+
+}
+
+void unsetEnVars(){
+
+}
+
+void alias(){
+
+}
+
+void unalias(){
+
 }
 
 void processCommands(FILE *pf, int interactive)
@@ -18,24 +54,41 @@ void processCommands(FILE *pf, int interactive)
     resetParser(pf);
     do {
         command *cmd;
-        if (interactive) issuePrompt(pf);
+        if (interactive) issuePrompt(stdout);
         first = cmd = nextCommand(&eof);
         while (cmd) {
-            printCommand(cmd, stdout); 
+            //Check all built-in commands first
+            if (strcmp(cmd->zCmd, "cd") == 0){
+                changeDirectory(cmd);
+            }else if(strcmp(cmd->zCmd, "setenv") == 0){
+                setEnVars();
+            }else if(strcmp(cmd->zCmd, "unsetenv") == 0){
+                unsetEnVars();
+            }else if(strcmp(cmd->zCmd, "alias") == 0){
+                alias();
+            }else if(strcmp(cmd->zCmd, "unalias") == 0){
+                unalias();
+            }else if(strcmp(cmd->zCmd, "quit") == 0){
+                exit(0);
+            }else{
+                //if the command given is not a built in command, run it.
+
+            }
+            //fprintf(stdout, "\n");
 	    cmd = cmd->pcNext;
         }
         if (first) destroyCommand(first);
     } while (!eof);
 }
 
-int 
-main(int argc, char **argv)
-{
-#if 0
-	ishrc = fopen(.....)
-	processCommands(ishrc, 0);
-        fclose(ishrc);
-#endif
+int main(int argc, char **argv){
+    if(0){
+        //ishrc = fopen(.....)
+	    //processCommands(ishrc, 0);
+        //fclose(ishrc);
+    }
+    
+    gethostname(name, sizeof(name));
 	processCommands(stdin, 1);
 	return 0;
 }
