47bed28a9bce2e1bb4211a8394e672691d2e4aac
Patrick Bridges <patrickb314@gmail.com>
2025-02-17 14:29:16 -0700

    Fixed some merging that had pulled code from other students.

--- src-c/ish.c
+++ src-c/ish.c
@@ -1,57 +1,14 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <unistd.h>
-#include <string.h>
-#include <sys/wait.h>
-#include <pwd.h>
 
 #include "command.h"
-#include "parser.h"
-
-char name[256];
-struct passwd* pwd;
+#include "parser.tab.h"
 
 void
 issuePrompt(FILE *pf)
 {
-    fprintf(pf, "%s%%> ", name);
+    fprintf(pf, "> ");
     fflush(pf);
-}
-
-void applyRedirection(command *cmd){
-
-}
-
-void changeDirectory(command *cmd){
-    if(cmd->pa->nArgs == 1){
-        printf("Returning to home directory\n");
-        pwd = getpwent();
-        chdir(pwd->pw_dir);
-    }else{
-        if(cmd->pa->nArgs > 2){
-            fprintf(stderr, "Too many arguments\n");
-        }else if(chdir(cmd->pa->azArgs[1]) != 0){
-            perror("invalid path");
-        }
-    }
-}
-
-//requires a linked list data structure.
-void setEnVars(){
-
-}
-
-void unsetEnVars(){
-
-}
-
-//requires a map data structure
-void alias(){
-
-}
-
-void unalias(){
-
 }
 
 void processCommands(FILE *pf, int interactive)
@@ -61,51 +18,24 @@ void processCommands(FILE *pf, int interactive)
     resetParser(pf);
     do {
         command *cmd;
-        if (interactive) issuePrompt(stdout);
+        if (interactive) issuePrompt(pf);
         first = cmd = nextCommand(&eof);
         while (cmd) {
-            //Check all built-in commands first
-            if (strcmp(cmd->zCmd, "cd") == 0){
-                changeDirectory(cmd);
-            }else if(strcmp(cmd->zCmd, "setenv") == 0){
-                setEnVars();
-            }else if(strcmp(cmd->zCmd, "unsetenv") == 0){
-                unsetEnVars();
-            }else if(strcmp(cmd->zCmd, "alias") == 0){
-                alias();
-            }else if(strcmp(cmd->zCmd, "unalias") == 0){
-                unalias();
-            }else if(strcmp(cmd->zCmd, "quit") == 0){
-                exit(0);
-            }else{
-                //if the command given is not a builtin command, run it.
-                int pid = fork();
-                if(pid != 0){
-                    waitpid(pid, NULL, 0);
-                }else{
-                    //basic cmd execution, need to handle redirections
-                    //and environment passthrough variables.
-                    char* envp = NULL;
-                    //add string token to find the path for running the command
-                    //must handle relative and absolute paths for a given command
-                    execve(cmd->zCmd, cmd->pa->azArgs, &envp);
-                }
-            }
-            //fprintf(stdout, "\n");
+            printCommand(cmd, stdout); 
 	    cmd = cmd->pcNext;
         }
         if (first) destroyCommand(first);
     } while (!eof);
 }
 
-int main(int argc, char **argv){
-    if(0){
-        //ishrc = fopen(.....)
-	    //processCommands(ishrc, 0);
-        //fclose(ishrc);
-    }
-    
-    gethostname(name, sizeof(name));
+int 
+main(int argc, char **argv)
+{
+#if 0
+	ishrc = fopen(.....)
+	processCommands(ishrc, 0);
+        fclose(ishrc);
+#endif
 	processCommands(stdin, 1);
 	return 0;
 }
