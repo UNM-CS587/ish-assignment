2995af4f088c7f1a52c466a946e27459a5f4bb69
Andres Quan <aquan9@users.noreply.github.com>
2025-02-17 06:13:26 +0000

    Adding cd functionality

--- src/ish.cpp
+++ src/ish.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <ostream>
 #include <istream>
+#include <filesystem>
 
 namespace ish {
 
@@ -33,8 +34,53 @@ void processCommands(std::istream &istr, std::ostream& ostr, bool interactive)
                 //THIS IS WHERE ALL THE BUILT-INS NEED TO BE HANDLED.
                 //This is how  you get the command name
                 ostr << cmd.getName() << "\n";
+
                 if (cmd.getName() == "quit"){
                     exit(0);
+                }
+                
+                if (cmd.getName() == "cd"){
+                    //Figure out where we're going to cd to.
+                    
+                    //Check to see if cd has any arguments.
+                    if (cmd.getArguments().size() == 0){
+                        ostr << "No arguments for cd, I should cd home\n";
+                        //How do I figure out the home directory? Usually that's one of the environment vars... 
+                        std::string homedir = getenv("HOME");
+                        //Now I just need to use a syscall
+                        if(std::filesystem::exists(homedir.c_str())){
+                            if(std::filesystem::is_directory(homedir.c_str())){
+                                chdir(homedir.c_str());
+                            } else {
+                                "Error: Path is not a directory\n";
+                            }
+
+                        } else {
+                            "Error: Path does not exist\n";
+                        }
+                        continue;
+                    }
+
+                    //Check if cd has too many args.
+                    if (cmd.getArguments().size() > 1){
+                        ostr << "Too many arguments for cd. Usage: cd [dir]\n";
+                        continue;
+                    }
+
+                    std::string target_dir = cmd.getArguments()[0];
+                    ostr << "about to cd to: " << args << "\n";
+                    //gotta check to see if the path given is actually a directory and not just a regular file
+                    //also gotta check to see if the path is real. Need to validate it somehow. 
+                    if(std::filesystem::exists(target_dir.c_str())){
+                        if(std::filesystem::is_directory(target_dir.c_str())){
+                            chdir(target_dir.c_str());
+                        } else {
+                            "Error: Path is not a directory\n";
+                        }
+
+                    } else {
+                        "Error: Path does not exist\n";
+                    }
                 }
 
             }               
